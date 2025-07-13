0846da0351e62381817ca151fdbfbd262701986d
Aiautomation2025 <daniel@glwhso.com>
2025-07-12 20:31:10 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Welcome to Winbox UI</h1>
+      <Button>Click Me</Button>
+    </div>
+  );
+}
+
+export default App;
